63bd7b46ac1d4a2963af1e95b49f554ec5a56724
VeraPutriPriandini <veraandini546@gmail.com>
2026-04-20 16:03:48 +0700

    Update diary_card.dart
    update diary

--- lib/widgets/diary_card.dart
+++ lib/widgets/diary_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/mood_model.dart';
+import '../screens/diary_detail_screen.dart'; // tambahkan ini
 
 class DiaryCard extends StatelessWidget {
   final MoodModel diary;
@@ -7,42 +8,54 @@ class DiaryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 12),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(diary.emoji, style: const TextStyle(fontSize: 28)),
-          const SizedBox(width: 15),
-          Expanded(
-            // Requirement: Expanded agar teks tidak overflow
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  diary.hari,
-                  style: const TextStyle(fontSize: 11, color: Colors.grey),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  diary.catatan,
-                  style: const TextStyle(
-                    fontSize: 13,
-                    color: Colors.black87,
-                    height: 1.4,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
+    return InkWell(
+      // bikin card bisa diklik
+      onTap: () {
+        Navigator.push(//pindah halaman ke detail
+          context,
+          MaterialPageRoute(
+            builder: (context) => DiaryDetailScreen(diary: diary),
           ),
-        ],
+        );
+      },
+
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 12),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(diary.emoji, style: const TextStyle(fontSize: 28)),
+            const SizedBox(width: 15),
+
+            Expanded(//mengisi sisa ruangan yg sisa
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    diary.hari,
+                    style: const TextStyle(fontSize: 11, color: Colors.grey),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    diary.catatan,
+                    style: const TextStyle(
+                      fontSize: 13,
+                      color: Colors.black87,
+                      height: 1.4,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
